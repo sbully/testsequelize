2d7cbbea977c590382d9058de1e4593716cb2536
sbully <bully.sebastien39@gmail.com>
2021-05-04 18:24:40 +0200

    feat(route.js): add route to add members

--- app/model/labmembers.model.js
+++ app/model/labmembers.model.js
@@ -1,19 +1,23 @@
 const { Sequelize } = require('sequelize');
 const db = require('../services/dbconnectionservice');
 
-const LabMembers = db.define('labmembers', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
+const LabMembers = db.define(
+  'labmembers',
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    firstname: { type: Sequelize.STRING },
+    lastname: { type: Sequelize.STRING },
+    email: { type: Sequelize.STRING },
+    role: {
+      type: Sequelize.ENUM,
+      values: ['Admin', 'Member', 'User'],
+    },
   },
-  firstName: { type: Sequelize.STRING },
-  lastname: { type: Sequelize.STRING },
-  email: { type: Sequelize.STRING },
-  role: {
-    type: Sequelize.ENUM,
-    values: ['Admin', 'Member', 'User'],
-  },
-});
+  { timestamps: false }
+);
 
 module.exports = LabMembers;
--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const db = require('./services/dbconnectionservice');
+const labMembers = require('./model/labmembers.model');
 
-router.get('/', (req, res) => res.send('labmembers'));
+// get list of labmembers
+router.get('/', (req, res) =>
+  labMembers
+    .findAll()
+    .then((storedLabMembers) => {
+      console.log(storedLabMembers);
+      res.send(storedLabMembers);
+    })
+    .catch((err) => {
+      console.log(`Error: ${err}`);
+      res.sendStatus(401);
+    })
+);
+
+// add a labmembers
+router.get('/add', (req, res) => {
+  const data = {
+    firstname: 'Pierre',
+    lastname: 'Pierre',
+    email: 'pierre@gmail.com',
+    role: 'Member',
+  };
+
+  labMembers
+    .create({
+      firstname: data.firstname,
+      lastname: data.lastname,
+      email: data.email,
+      role: data.role,
+    })
+    .then((storedLabMembers) => res.redirect('/'))
+    .catch((err) => {
+      console.log(`error when create : ${err}`);
+    });
+});
 
 module.exports = router;
