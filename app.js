aba31b82d44f10d16bf6351cb7286512c99fcecf
sbully <bully.sebastien39@gmail.com>
2021-05-04 15:47:08 +0200

    feat(sequelizetest):create module for db connection

--- app.js
+++ app.js
@@ -2,14 +2,11 @@ const { Sequelize } = require('sequelize');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const db = require('./app/services/dbconnectionservice');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const db = new Sequelize('sequelizetest', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
 db.authenticate()
   .then(() => console.log('database connected ....'))
   .catch((err) => console.log(`unable to connect, Error :${err}`));
