02281fa9c8d2fdd9b368a3447dbad8690f0e89b2
sbully <bully.sebastien39@gmail.com>
2021-06-02 16:19:30 +0200

    fix(router): fix cross origin requete

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ db.authenticate()
   .catch((err) => console.log(`unable to connect, Error :${err}`));
 
 app.use(express.json()); //  <<== NE PAS OUBLIER
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 app.use('/', routes);
 
 app.listen(PORT, console.log(`server started on port :${PORT}`));
